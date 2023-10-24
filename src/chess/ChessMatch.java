e15bcf9fa026cde101734096853be1155cdee319
William Devide Komel <williamdevide@gmail.com>
2023-10-24 11:45:53 -0300

    Class: ChessException e ChessPosition (ChessException e ChessPosition )

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,9 +32,22 @@ public class ChessMatch {
 		//board.placePiece(new King(board, Color.BLACK), new Position(0,4));
 		//board.placePiece(new King(board, Color.WHITE), new Position(7,4));
 
-		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		//placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+		//placeNewPiece('e', 8, new King(board, Color.BLACK));
+		//placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));
 		
 		
 	}
