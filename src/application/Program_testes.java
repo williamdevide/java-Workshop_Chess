514be7400ec3c518c1d449dee4525c34020f89cf
William Devide Komel <williamdevide@gmail.com>
2023-10-24 09:38:54 -0300

    Class: Board e Piece

---
+++ src/application/Program_testes.java
@@ -0,0 +1,21 @@
+package application;
+
+import boardgame.Board;
+import boardgame.Position;
+
+public class Program_testes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// teste da classe POSITION
+		Position pos = new Position(3,5);
+		System.out.println(pos);
+		
+		// teste da classe BOARD e PIECE
+		Board borad = new Board(8, 8);
+		
+		
+	}
+
+}
