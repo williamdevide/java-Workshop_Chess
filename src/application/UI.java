30cd70434c579b8cb096c6cf3d9a249ec7b62f33
William Devide Komel <williamdevide@gmail.com>
2023-10-25 09:12:28 -0300

    Class: ChessMatch e UI (CheckMate logic)

--- src/application/UI.java
+++ src/application/UI.java
@@ -57,16 +57,15 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn : " + chessMatch.getTurn());
-		//if (!chessMatch.getCheckMate()) {
-			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+		if (!chessMatch.getCheckMate()) {
+			System.out.println("Aguardando jogador: " + chessMatch.getCurrentPlayer());
 			if (chessMatch.getCheck()) {
 				System.out.println("CHECK!");
 			}
-		//}
-		//else {
-		//	System.out.println("CHECKMATE!");
-		//	System.out.println("Winner: " + chessMatch.getCurrentPlayer());
-		//}
+		} else {
+			System.out.println("CHECKMATE!");
+			System.out.println("Vencedor: " + chessMatch.getCurrentPlayer());
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
